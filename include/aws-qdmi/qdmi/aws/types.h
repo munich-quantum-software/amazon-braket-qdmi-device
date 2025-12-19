f8c569ecb262b8ee221ffe3b56322d40b7c9c01f
flowerthrower <flowerthrower@users.noreply.github.com>
2025-12-19 18:34:10 +0100

    🚧 remove non-ci adds

--- include/aws-qdmi/qdmi/aws/types.h
+++ include/aws-qdmi/qdmi/aws/types.h
@@ -1,12 +1,3 @@
-/*------------------------------------------------------------------------------
-Copyright 2025 AWS QDMI Device Implementation
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-
-SPDX-License-Identifier: MIT
-------------------------------------------------------------------------------*/
-
 /** @file
  * @brief Defines AWS Braket-specific types for the QDMI device interface.
  */
