0ced296af921a8944adf244aa8f4375cdfe23533
flowerthrower <flowerthrower@users.noreply.github.com>
2026-01-20 15:34:29 +0100

    🚧 debug CI

--- cmake/ExternalDependencies.cmake
+++ cmake/ExternalDependencies.cmake
@@ -26,9 +26,9 @@ if(BUILD_TESTS)
 endif()
 
 # cmake-format: off
-set(QDMI_VERSION 1.2.0
+set(QDMI_VERSION 1.2.1
         CACHE STRING "QDMI version")
-set(QDMI_REV "3da94506d365963db8b1e419488858cfa6371d2d" # v1.2.0
+set(QDMI_REV "70b815615475598c6194096a29c1b2340dd54a6c" # v1.2.x
         CACHE STRING "QDMI identifier (tag, branch or commit hash)")
 set(QDMI_REPO_OWNER "Munich-Quantum-Software-Stack"
         CACHE STRING "QDMI repository owner (change when using a fork)")
