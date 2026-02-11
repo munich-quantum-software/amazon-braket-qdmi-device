9f709f8629fcfd44018c689e9b78761ee649cb61
flowerthrower <flowerthrower@users.noreply.github.com>
2026-02-11 13:23:27 +0100

    🚧 debug windows CI

--- cmake/ExternalDependencies.cmake
+++ cmake/ExternalDependencies.cmake
@@ -44,6 +44,30 @@ FetchContent_Declare(
   FIND_PACKAGE_ARGS ${QDMI_VERSION})
 list(APPEND FETCH_PACKAGES qdmi)
 
+if(WIN32 AND NOT DEFINED AWSSDK_FLAGS_SET)
+  # AWS SDK CI / Windows defaults
+  if(NOT DEFINED USE_SHARED_CRT)
+    set(USE_SHARED_CRT
+        ON
+        CACHE BOOL "" FORCE)
+  endif()
+
+  if(NOT DEFINED ENABLE_UNITY_BUILD)
+    set(ENABLE_UNITY_BUILD
+        ON
+        CACHE BOOL "" FORCE)
+  endif()
+
+  if(NOT DEFINED BUILD_SHARED_LIBS)
+    set(BUILD_SHARED_LIBS
+        OFF
+        CACHE BOOL "" FORCE)
+  endif()
+  set(AWSSDK_FLAGS_SET
+      TRUE
+      CACHE INTERNAL "" FORCE)
+endif()
+
 set(AWSSDK_VERSION
     1.11.714
     CACHE STRING "AWS SDK version")
